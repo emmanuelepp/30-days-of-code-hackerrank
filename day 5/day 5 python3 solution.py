c3b77e832cafd9abddc482f82ecad3a988bfc917
emmanuelepp <Emmanuelponciano@gmail.com>
2020-11-14 00:21:51 -0500

    add python solution

---
+++ day 5/day 5 python3 solution.py
@@ -0,0 +1,11 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    i = 1
+    for i in range(1, 11):
+        print(f'{n} x {i} =  {n * i}')
